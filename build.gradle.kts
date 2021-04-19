165a3dc97a32c38f3d7e8dd3eff67b4bff6965bc
Eric Fenderbosch <eric@hubindustrial.com>
2021-04-19 08:15:43 -0400

    version bumps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.30"
+    kotlin("jvm") version "1.4.32"
 }
 
 group = "org.example"
@@ -13,10 +13,10 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("software.amazon.awssdk:s3:2.16.17")
-    implementation("org.slf4j:slf4j-api:1.7.30")
-    runtimeOnly("ch.qos.logback:logback-classic:1.2.3")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    implementation("software.amazon.awssdk:s3:2.16.+")
+    implementation("org.slf4j:slf4j-api:1.7.+")
+    runtimeOnly("ch.qos.logback:logback-classic:1.2.+")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.+")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
